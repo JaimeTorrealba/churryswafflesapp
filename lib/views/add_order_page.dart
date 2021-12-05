702c730ecd40b250215d9a76b092fbee62c62ab6
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2021-12-05 11:26:06 -0300

    add form creating order

--- lib/views/add_order_page.dart
+++ lib/views/add_order_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import '../components/commons/churrysTitle.dart';
+import '../components/commons/churrys_title.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+const initialQuantity = {'quantity': 0};
 
 class AddOrderPage extends StatefulWidget {
   static const String id = '/addOrder';
@@ -8,40 +11,110 @@ class AddOrderPage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<AddOrderPage> {
-  List<Widget> cheader = [
-    Text(
-      'asdsad',
-    ),
-    Text(
-      '23232 \$',
-    ),
-    Text(
-      '4',
-    )
-  ];
+  Map<String, dynamic> newOrder = {
+    'totalQuantity': 0,
+    'totalPrice': 0,
+  };
+  int totalQuantity = 0;
+  late dynamic productsData;
+
+  // final CollectionReference _collectionRef =
+  //     FirebaseFirestore.instance.collection('products');
+  //
+  // Future<void> getData() async {
+  //   // Get docs from collection reference
+  //   QuerySnapshot querySnapshot = await _collectionRef.get();
+  //
+  //   // Get data from docs and convert map to List
+  //   productsData = querySnapshot.docs.map((doc) => doc.data()).toList();
+  //   for (var obj in productsData) {
+  //     obj["quantity"] = 0;
+  //   }
+  //   print(productsData);
+  // }
+
+  void getData() {
+    productsData = [
+      {"Name": 'Juan1', "Price": 1000, 'Quantity': 0},
+      {"Name": 'Juan2', "Price": 2000, 'Quantity': 0},
+      {"Name": 'Juan3', "Price": 3000, 'Quantity': 0},
+    ];
+    print(productsData);
+  }
+
+  @override
+  void initState() {
+    getData();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Add new order'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TitleAddProducts(),
+              for (int i = 0; i < productsData.length; i++)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      productsData[i]['Name'],
+                    ),
+                    Text(
+                      productsData[i]['Price'].toString(),
+                    ),
+                    SizedBox(
+                      width: 75,
+                      child: TextFormField(
+                        onChanged: (value) {
+                          productsData[i]['Quantity'] = value;
+                          updateNewOrder(i, value);
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Cantidad',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Text('Total:'),
+                Text(newOrder['totalQuantity'].toString())
+              ])
+            ],
+          ),
+        ));
+  }
+
+  dynamic updateNewOrder(key, value) {
+    setState(() {
+      newOrder['totalQuantity'] = newOrder['totalQuantity'] + int.parse(value);
+    });
+
+    print(productsData[key]);
+    print(newOrder['totalQuantity']);
+    print(value);
+  }
+}
+
+class TitleAddProducts extends StatelessWidget {
+  const TitleAddProducts({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add new order'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            ChurrysTitle(titleText: 'Product'),
-            ChurrysTitle(titleText: 'Price'),
-            ChurrysTitle(titleText: 'Quantity')
-          ]),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: cheader),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            ChurrysTitle(titleText: 'Total:'),
-          ])
-        ]),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      ChurrysTitle(titleText: 'Product'),
+      ChurrysTitle(titleText: 'Price'),
+      ChurrysTitle(titleText: 'Quantity')
+    ]);
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../components/home/side_bar.dart';
+import 'package:churrys_waffles/views/add_order_page.dart';
 
 class MyHomePage extends StatefulWidget {
   static const String id = '/';
@@ -9,18 +10,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final CollectionReference _collectionRef =
-      FirebaseFirestore.instance.collection('products');
+  // final CollectionReference _collectionRef =
+  //     FirebaseFirestore.instance.collection('products');
 
-  Future<void> getData() async {
-    // Get docs from collection reference
-    QuerySnapshot querySnapshot = await _collectionRef.get();
-
-    // Get data from docs and convert map to List
-    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
-    print('se ejecuta el getData');
-    print(allData);
-  }
+  // Future<void> getData() async {
+  //   // Get docs from collection reference
+  //   QuerySnapshot querySnapshot = await _collectionRef.get();
+  //
+  //   // Get data from docs and convert map to List
+  //   final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+  //   print('se ejecuta el getData');
+  //   print(allData);
+  // }
 
   @override
   // void initState() {
@@ -34,7 +35,12 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Churry\'s Waffles App'),
       ),
       drawer: SideBar(),
-      // floatingActionButton: ,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).pushNamed(AddOrderPage.id);
+        },
+      ),
     );
   }
 }
